3a46a1022d823891fa6f16b748f7ccbb795fc2ed
Arvind Varma <av894883@gmail.com>
2022-10-15 17:56:17 +0530

    yes

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express =  require("express")
+const cor = require("cors")
 const mongoose = require("mongoose")
 const gitAction = require("./gitActionSchema")
 const app = express();
 app.use(express.json());
 
-const dataBaseUrl='mongodb+srv://Arvind:Arvind_varma@cluster0.yqwdumn.mongodb.net/?retryWrites=true&w=majority'
+ 
+const dataBaseUrl='mongodb+srv://789789789@cluster0.7cu4fig.mongodb.net/?retryWrites=true&w=majority'
 
-//  mongoose.connect(dataBaseUrl).then(()=>{
-//     console.log("dataBase is conneted")
-//  }).catch((err)=>{
-//     console.log("error" +err)
-//  })
+ 
 
 app.listen(3000,()=>{
 console.log("Server is started");
@@ -24,9 +22,7 @@ app.get("/",(req,res)=>{
     res.send("home")
 })
 
-app.get("/push",(req,res)=>{
-    res.status(200).json({massages:"send"})
-})
 app.post("/push",(req,res)=>{
     res.status(200).json({massages:"send"})
 })
+ 
