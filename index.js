2b496e488b0492442290e96baf6100feb15ccc2c
Arvind Varma <av894883@gmail.com>
2022-10-15 16:14:22 +0530

    yes

--- index.js
+++ index.js
@@ -6,69 +6,15 @@ app.use(express.json());
 
 const dataBaseUrl='mongodb+srv://Arvind:Arvind_varma@cluster0.yqwdumn.mongodb.net/?retryWrites=true&w=majority'
 
- mongoose.connect(dataBaseUrl).then(()=>{
-    console.log("dataBase is conneted")
- }).catch((err)=>{
-    console.log("error" +err)
- })
+//  mongoose.connect(dataBaseUrl).then(()=>{
+//     console.log("dataBase is conneted")
+//  }).catch((err)=>{
+//     console.log("error" +err)
+//  })
 
-app.listen(2300,()=>{
+app.listen(3000,()=>{
 console.log("Server is started");
-})
 
-app.post("/push",async(req,res)=>{
-    try{
-        const {request_id, author, branch, createdAt} = req.body;
-        console.log(req.body);
-        await gitAction.create({
-            request_id,
-            author,
-            action: "push",
-            to_branch: branch,
-            timestamp: createdAt
-        })
-        res.json({message: "success"})
-    }catch(err){
-        throw new Error(err)
-    }
-})
+});
 
-app.post("/pull",async(req,res)=>{
-    try{
-        const {request_id, author, from_branch, to_branch, createdAt} = req.body;
-        console.log(req.body);
-        await gitAction.create({
-            request_id,
-            author,
-            action: "pull_request",
-            from_branch,
-            to_branch,
-            timestamp: createdAt
-        })
-        res.json({message: "success"})
-    }catch(err){
-        throw new Error(err)
-    }
- })
-
- app.post("/marge",async(req,res)=>{
-    try{
-        const {request_id, author, from_branch, to_branch, createdAt} = req.body;
-        console.log(req.body);
-        await gitAction.create({
-            request_id,
-            author,
-            action: "merge_pr",
-            from_branch,
-            to_branch,
-            timestamp: createdAt
-        })
-        res.json({message: "success"})
-
-    }catch(err){
-        throw new Error(err)
-    }
- })
-
-
- 
+console.log("hrello")
